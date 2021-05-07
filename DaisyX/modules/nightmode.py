8678f185a546def7c896bced9105888d9f058474
rihan2575 <83025633+rihan2575@users.noreply.github.com>
2021-05-07 12:32:17 +0530

    Update nightmode.py

--- DaisyX/modules/nightmode.py
+++ DaisyX/modules/nightmode.py
@@ -113,7 +113,7 @@ async def job_close():
         try:
             await tbot.send_message(
                 int(warner.chat_id),
-                "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @DaisyXbot**",
+                "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @ShinchanHelper_Bot**",
             )
             await tbot(
                 functions.messages.EditChatDefaultBannedRightsRequest(
@@ -143,7 +143,7 @@ async def job_open():
         try:
             await tbot.send_message(
                 int(warner.chat_id),
-                "`06:00 Am, Group Is Opening.`\n**Powered By @DaisyXBot**",
+                "`06:00 Am, Group Is Opening.`\n**Powered By @ShinchanHelper_Bot**",
             )
             await tbot(
                 functions.messages.EditChatDefaultBannedRightsRequest(
