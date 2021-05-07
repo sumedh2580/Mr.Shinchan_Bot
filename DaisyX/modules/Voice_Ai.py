3fa2f5abb6c14103e4c12463febf1cbdd32c3f92
rihan2575 <83025633+rihan2575@users.noreply.github.com>
2021-05-07 11:26:34 +0530

    Update Voice_Ai.py

--- DaisyX/modules/Voice_Ai.py
+++ DaisyX/modules/Voice_Ai.py
@@ -76,7 +76,7 @@ async def _(event):
         res = get(server)
         if "Wolfram Alpha did not understand" in res.text:
             await event.reply(
-                "Sorry, Daisy's AI systems could't recognized your question.."
+                "Sorry, Shinchan's AI systems could't recognized your question.."
             )
             return
         await event.reply(f"**{i}**\n\n" + res.text, parse_mode="markdown")
@@ -117,7 +117,7 @@ async def _(event):
 
                     if "Wolfram Alpha did not understand" in res:
                         answer = (
-                            "I'm sorry Daisy's AI system can't undestand your problem"
+                            "I'm sorry Shinchan's AI system can't undestand your problem"
                         )
                     else:
                         answer = res.text
@@ -145,7 +145,7 @@ async def _(event):
                     transcript_response == "Wolfram Alpha did not understand your input"
                 ):
                     try:
-                        answer = "Sorry, Daisy's AI system can't understand you.."
+                        answer = "Sorry, Shinchan's AI system can't understand you.."
                         tts = gTTS(answer, tld="com", lang="en")
                         tts.save("results.mp3")
                     except AssertionError:
