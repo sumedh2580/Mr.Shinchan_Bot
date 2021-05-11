98348059ed49a2330c09765883409210946ec3e7
sumedh2580 <82429530+sumedh2580@users.noreply.github.com>
2021-05-11 17:39:39 +0530

    Update fakeit.py

--- DaisyX/modules/fakeit.py
+++ DaisyX/modules/fakeit.py
@@ -59,7 +59,7 @@ async def _(event):
             with open("FRIDAYOT.jpg", "wb") as f:
                 f.write(response.content)
 
-        captin = f"Fake Image powered by @Mr_Shinchan_Support."
+        captin = f"Fake Image powered by @cuddles_of_pandas."
         fole = "FRIDAYOT.jpg"
         await tbot.send_file(event.chat_id, fole, caption=captin)
         await event.delete()
