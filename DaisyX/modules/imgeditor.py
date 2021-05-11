19d2bf3561581703668e54bee6d7a5c3085de3bf
sumedh2580 <82429530+sumedh2580@users.noreply.github.com>
2021-05-11 17:43:22 +0530

    Update imgeditor.py

--- DaisyX/modules/imgeditor.py
+++ DaisyX/modules/imgeditor.py
@@ -447,7 +447,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 __mod_name__ = "Image Editor"
 __help__ = """
 <b> IMAGE EDITOR </b>
-Shinchan have some advanced image editing tools inbuilt
+King Of Darkness have some advanced image editing tools inbuilt
 Bright, Circle, RemBG, Blur, Border, Flip, Glitch, Sticker maker and more
 
 - /edit [reply to image]: Open the image editor
